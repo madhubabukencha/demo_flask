5aaf06432b6367c5cee69b67dd810ba5c62a5f36
Madhu Babu Kencha <39263940+madhubabukencha@users.noreply.github.com>
2022-02-24 22:06:55 +0530

    creating hello.py app

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def hello_world():
+    return "<h1>My First Flask App</h1>"
+
+@app.route("/wishme")
+def wishme():
+    return "<h1>Hello Madhu, Welcome to Flask</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
